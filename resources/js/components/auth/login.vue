ba08b77f2f1bae4c0a924d4b0b7f6ceef8807b70
BlueVortex123 <duicu.andrei@gmail.com>
2021-07-25 00:03:52 +0300

    add Register and SweetAlert

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -16,9 +16,12 @@
                     <div class="form-group">
                       <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
                         placeholder="Enter Email Address" v-model="form.email">
+                        <small class="text-danger" v-if="errors.email">{{ errors.email[0] }} </small>
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control" id="exampleInputPassword" v-model="form.password" placeholder="Password">
+                      <small class="text-danger" v-if="errors.password">{{ errors.password[0] }} </small>
+
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
@@ -47,7 +50,7 @@
         </div>
       </div>
     </div>
-   
+    
    
     </div>
 </template>
@@ -55,20 +58,45 @@
 <script>
 
 export default{
+    created(){
+        if(User.loggedIn()){
+          this.$router.push({name: 'home'})
+        }
+    },
+    
     data(){
         return {
           form:{
             email:null,
             password:null
+          },
+
+          errors:{
+
           }
         }
       },
 
       methods: {
-        login(){
+       
+       login(){
           axios.post('/api/auth/login', this.form)
-          .then(res => User.responseAfterLogin(res))
-          .catch(error => console.log(error.response.data));
+          .then(res => {
+            User.responseAfterLogin(res)
+
+            Toast.fire({
+              icon: 'success',
+              title: 'Signed in successfully'
+            });
+            
+            this.$router.push({ name: 'home' })
+          })
+          .catch(error => this.errors = error.response.data.errors)
+          .catch(
+            Toast.fire({
+              icon: 'warning',
+              title: 'Invalid Email or Password'
+            }))
         }  
       },
 }
--- resources/js/components/auth/register.vue
+++ resources/js/components/auth/register.vue
@@ -10,19 +10,27 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Register</h1>
                   </div>
-                  <form>
+                  <form class="user" @submit.prevent="signup">
                     <div class="form-group">
-                      <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter Your Full Name">
+                      <input type="text" class="form-control" id="exampleInputFirstName" placeholder="Enter Your Full Name" v-model="form.name">
+                      <small class="text-danger" v-if="errors.name">{{ errors.name[0] }} </small>
+
                     </div>
                     <div class="form-group">
-                      <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp"
+                      <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" v-model="form.email"
                         placeholder="Enter Email Address">
+                        <small class="text-danger" v-if="errors.email">{{ errors.email[0] }} </small>
+
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" v-model="form.password">
+                      <small class="text-danger" v-if="errors.password">{{ errors.password[0] }} </small>
+
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control" id="exampleInputPasswordRepeat" placeholder="Confirm Password">
+                      <input type="password" class="form-control" id="exampleInputPasswordRepeat" placeholder="Confirm Password" v-model="form.password_confirmation">
+                      <small class="text-danger" v-if="errors.password_confirmation">{{ errors.password_confirmation[0] }} </small>
+
                     </div>
                     <div class="form-group">
                       <button type="submit" class="btn btn-primary btn-block">Register</button>
@@ -45,12 +53,50 @@
     </div>
 </template>
 
-<script type = "text/javascript">
+<script>
 
-export default {
+export default{
+    created(){
+        if(User.loggedIn()){
+          this.$router.push({name: 'home'})
+        }
+    },
     
+    data(){
+        return {
+          form:{
+            name: null,
+            email:null,
+            password:null,
+            confirm_password: null
+          },
 
+          errors:{
+
+          }
+        }
+      },
+
+      methods: {
+       
+       signup(){
+          axios.post('/api/auth/signup', this.form)
+          .then(res => {
+            User.responseAfterLogin(res)
+
+            Toast.fire({
+              icon: 'success',
+              title: 'Signed in successfully'
+            });
+            
+            this.$router.push({ name: 'home' })
+          })
+          .catch(error => this.errors = error.response.data.errors)
+         
+        }  
+      },
 }
+  
 </script>
 
 <style type="text/css">
